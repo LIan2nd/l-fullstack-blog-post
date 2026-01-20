e354c4fe9fb9bdeec44cf3fb654709291e9bc0c9
LIan2nd <alfiannurusyaid19@gmail.com>
2026-01-20 14:33:55 +0700

    feat(talent-growth): init n setup project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+import MainLayout from "./features/posts/layout/main"
+
+function App() {
+
+  return (
+    <>
+      <MainLayout>
+        <Button>Misal inimah</Button>
+      </MainLayout>
+    </>
+  )
+}
+
+export default App
