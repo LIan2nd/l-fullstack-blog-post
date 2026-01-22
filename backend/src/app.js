f0e72331fa5e20994c4161b31a13745496377f08
LIan2nd <alfiannurusyaid19@gmail.com>
2026-01-22 12:13:12 +0700

    feat(talent-growth): change deployment plan to vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -47,8 +47,11 @@ app.use('/api', commentRoutes);
 // User Routes
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+export default app;
